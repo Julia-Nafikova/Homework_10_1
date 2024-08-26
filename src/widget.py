eda3595c22d13c75059cc7334131c06225106922
Nafikova_Julia <nafikova91@yandex.ru>
2024-08-26 14:37:14 +0300

    fixing the function get_date

--- src/widget.py
+++ src/widget.py
@@ -16,10 +16,12 @@ def mask_account_card(data_for_processing: str) -> str | None:
     else:
         return f"{name_card} {get_mask_card_number(number)}"
 
+
 def get_date(date: str) -> str | None:
     """ Date format change """
-
-
-
-
-#print(mask_account_card("Visa Platinum 7000792289606361"))
+    date_slice = date[0:10]
+    date_list = date_slice.split("-")
+    date_list_reverse = []
+    date_list_reverse.extend([date_list[2], date_list[1], date_list[0]])
+    date_change = '.'.join(date_list_reverse)
+    return date_change
