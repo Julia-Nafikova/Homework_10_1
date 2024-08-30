415cd30249f246097c1e4e49ba1d7b6d4e35532f
Nafikova_Julia <nafikova91@yandex.ru>
2024-08-30 15:42:33 +0300

    renamed variables in processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,12 @@
 def filter_by_state(list_dict: list[dict], state = "EXECUTED") -> list[dict]:
     """ Функция которая отбирает словари из списка по указанному значению"""
-    new_list_dict_state = []
+    filtered_list = []
     for dictionary in list_dict:
         if dictionary['state'] == state:
-            new_list_dict_state.append(dictionary)
-    return new_list_dict_state
+            filtered_list.append(dictionary)
+    return filtered_list
 
-def sort_by_date(list_dict: list[dict], date = True) -> list[dict]:
+def sort_by_date(list_dict: list[dict], is_descending = True) -> list[dict]:
     """ Функция которая сортирует словари из списка по дате"""
-    new_list_dict_date = sorted(list_dict, key=lambda x: x['date'], reverse=date)
-    return new_list_dict_date
+    sorted_list = sorted(list_dict, key=lambda x: x['date'], reverse=is_descending)
+    return sorted_list
