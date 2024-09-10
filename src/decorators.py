99f38dcc61d7921d5ff8d877ad62db162aa5d897
Nafikova_Julia <nafikova91@yandex.ru>
2024-09-10 16:19:15 +0300

    add new module test_decorators

--- src/decorators.py
+++ src/decorators.py
@@ -5,7 +5,7 @@ from functools import wraps
 def log(filename = None):
     "Декоратор, который логирует вызов функции и ее результат в файл или консоль"
     def wrapper(function):
-        @wraps(function)
+        # @wraps(function)
         def inner(*args, **kwargs):
             result = None
             try:
@@ -16,8 +16,11 @@ def log(filename = None):
                 log_str = f"{function.__name__} ok\n"
             finally:
                 if filename:
-                    with open(filename, "a") as file:
+                    path_to_file = os.path.join(os.path.dirname(__file__), "../logs", filename)
+                    with open(path_to_file, "w", encoding="utf-8") as file:
                         file.write(log_str)
+                    # with open(filename, "a") as file:
+                    #     file.write(log_str)
                 else:
                     print(log_str)
             return result
@@ -30,12 +33,12 @@ def log(filename = None):
 #
 # my_function("1", 2)
 
-# @log(filename="mylog.txt")
-# def my_function(x, y):
-#     "Функция которая выдает сумму сложения двух чисел"
-#     return x + y
-#
-# my_function("1", 2)
+@log(filename="mylog.txt")
+def my_function(x, y):
+    "Функция которая выдает сумму сложения двух чисел"
+    return x + y
+
+my_function(1, 2)
 
 
 
---
+++ tests/test_decorators.py
@@ -0,0 +1,57 @@
+import os.path
+import tempfile
+
+import pytest
+from src.decorators import log
+
+
+def test_log_ok_console(capsys):
+    @log()
+    def my_function(x, y):
+        return x + y
+
+    result = my_function(1, 2)
+    captured = capsys.readouterr()
+    assert captured.out == 'my_function ok\n\n'
+    assert result == 3
+
+def test_log_exception_console(capsys):
+    @log()
+    def my_function(x, y):
+        return x + y
+
+    result = my_function("1", 2)
+    captured = capsys.readouterr()
+    assert """my_function error: can only concatenate str (not "int") to str Inputs: ('1', 2)"""
+
+
+def test_log_ok_file(capsys):
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        log_file_path = tmp_file.name
+
+    @log(filename=log_file_path)
+    def my_function(x, y):
+        return x + y
+
+    result = my_function(1, 2)
+
+    with open(log_file_path, "r", encoding="utf-8") as file:
+        logs = file.read()
+
+    assert "my_function ok" in logs
+
+
+def test_log_exception_file():
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        log_file_path = tmp_file.name
+
+    @log(filename=log_file_path)
+    def my_function(x, y):
+        return x + y
+
+    result = my_function("1", 2)
+
+    with open(log_file_path, "r", encoding="utf-8") as file:
+        logs = file.read()
+
+    assert """my_function error: can only concatenate str (not "int") to str Inputs: ('1', 2)""" in logs
